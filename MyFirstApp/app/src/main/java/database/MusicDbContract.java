3e013daaed45ef4cd414374bf30bb819792fe424
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-09-05 14:20:28 +0200

    Updated access modifiers

--- MyFirstApp/app/src/main/java/database/MusicDbContract.java
+++ MyFirstApp/app/src/main/java/database/MusicDbContract.java
@@ -8,44 +8,44 @@ public final class MusicDbContract {
     private MusicDbContract(){}
 
     public static class FeedEntry implements BaseColumns {
-        public static final String TABLE_NAME_PLAYLIST = "Playlist";
-        public static final String TABLE_NAME_SONG = "Song";
-        public static final String TABLE_NAME_PLAYLIST_SONG = "Playlist_Song";
-        public static final String TABLE_NAME_ARTIST = "Artist";
-        public static final String TABLE_NAME_ARTIST_SONG = "Artist_Song";
+        static final String TABLE_NAME_PLAYLIST = "Playlist";
+        static final String TABLE_NAME_SONG = "Song";
+        static final String TABLE_NAME_PLAYLIST_SONG = "Playlist_Song";
+        static final String TABLE_NAME_ARTIST = "Artist";
+        static final String TABLE_NAME_ARTIST_SONG = "Artist_Song";
 
-        public static final String COLUMN_NAME_TITLE = "Title";
-        public static final String COLUMN_NAME_NAME = "Name";
-        public static final String COLUMN_NAME_SONG_PATH = "Path";
+        static final String COLUMN_NAME_TITLE = "Title";
+        static final String COLUMN_NAME_NAME = "Name";
+        static final String COLUMN_NAME_SONG_PATH = "Path";
 
-        public static final String KEY_NAME_PLAYLIST = "Playlist_Id";
-        public static final String KEY_NAME_SONG = "Song_Id";
-        public static final String KEY_NAME_ARTIST = "Artist_Id";
+        static final String KEY_NAME_PLAYLIST = "Playlist_Id";
+        static final String KEY_NAME_SONG = "Song_Id";
+        static final String KEY_NAME_ARTIST = "Artist_Id";
     }
 
-    public static final String CREATE_TABLE_PLAYLIST =
+    private static final String CREATE_TABLE_PLAYLIST =
             "CREATE TABLE " + FeedEntry.TABLE_NAME_PLAYLIST + " (" +
                     FeedEntry.KEY_NAME_PLAYLIST + " INTEGER PRIMARY KEY, " +
                     FeedEntry.COLUMN_NAME_TITLE + " TEXT) ";
 
-    public static final String CREATE_TABLE_SONG =
+    private static final String CREATE_TABLE_SONG =
             "CREATE TABLE " + FeedEntry.TABLE_NAME_SONG + "(" +
                     FeedEntry.KEY_NAME_SONG + " INTEGER PRIMARY KEY, " +
                     FeedEntry.COLUMN_NAME_TITLE + " TEXT) ";
 
-    public static final String CREATE_TABLE_ARTIST =
+    private static final String CREATE_TABLE_ARTIST =
             "CREATE TABLE " + FeedEntry.TABLE_NAME_ARTIST + " (" +
                     FeedEntry.KEY_NAME_ARTIST + " INTEGER PRIMARY KEY, " +
                     FeedEntry.COLUMN_NAME_NAME + " TEXT, " +
                     FeedEntry.COLUMN_NAME_SONG_PATH + " TEXT) ";
 
-    public static final String CREATE_TABLE_PLAYLIST_SONG =
+    private static final String CREATE_TABLE_PLAYLIST_SONG =
             "CREATE TABLE " + FeedEntry.TABLE_NAME_PLAYLIST_SONG + " (" +
                     FeedEntry._ID + " INTEGER PRIMARY KEY, " +
                     FeedEntry.KEY_NAME_PLAYLIST + " INTEGER, " +
                     FeedEntry.KEY_NAME_SONG + " INTEGER) ";
 
-    public static final String CREATE_TABLE_ARTIST_SONG =
+    private static final String CREATE_TABLE_ARTIST_SONG =
             "CREATE TABLE " + FeedEntry.TABLE_NAME_ARTIST_SONG + " (" +
                     FeedEntry._ID + " INTEGER PRIMARY KEY, " +
                     FeedEntry.KEY_NAME_ARTIST + " INTEGER, " +
