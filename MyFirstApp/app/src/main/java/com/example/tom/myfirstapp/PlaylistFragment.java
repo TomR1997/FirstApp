1c6810cbad0546521228ffa7a00faba215f33df4
Roelofs,Tom T <t.roelofs@student.fontys.nl>
2018-09-07 12:23:55 +0200

    Added RecyclerView with custom adapter

--- MyFirstApp/app/src/main/java/com/example/tom/myfirstapp/PlaylistFragment.java
+++ MyFirstApp/app/src/main/java/com/example/tom/myfirstapp/PlaylistFragment.java
@@ -2,15 +2,36 @@ package com.example.tom.myfirstapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import adapter.PlaylistAdapter;
+import dao.PlaylistDAO;
+import domain.Playlist;
+
 public class PlaylistFragment extends Fragment{
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private PlaylistDAO playlistDao;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.playlisttab, container, false);
-        return rootView;
+        View fragmentView = inflater.inflate(R.layout.playlisttab, container, false);
+
+        recyclerView = fragmentView.findViewById(R.id.playlistView);
+        layoutManager = new LinearLayoutManager(fragmentView.getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        playlistDao = new PlaylistDAO(fragmentView.getContext());
+
+        adapter = new PlaylistAdapter(playlistDao.getAll());
+        recyclerView.setAdapter(adapter);
+
+        return fragmentView;
     }
 }
--- MyFirstApp/app/src/main/java/dao/PlaylistDAO.java
+++ MyFirstApp/app/src/main/java/dao/PlaylistDAO.java
@@ -2,7 +2,11 @@ package dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import database.MusicDbContract;
 import database.MusicDbHelper;
@@ -26,5 +30,20 @@ public class PlaylistDAO {
 
     public boolean delete(long id){
         return db.delete(MusicDbContract.FeedEntry.TABLE_NAME_PLAYLIST, MusicDbContract.FeedEntry.KEY_NAME_PLAYLIST + " = " + id, null) > 0;
+    }
+
+    public List<Playlist> getAll(){
+       Cursor cursor = db.rawQuery("SELECT * FROM " + MusicDbContract.FeedEntry.TABLE_NAME_PLAYLIST, null);
+       List<Playlist> playlists = new ArrayList<>();
+
+       if(cursor.moveToFirst()){
+           while (cursor.moveToNext()){
+               String title = cursor.getString(cursor.getColumnIndex(MusicDbContract.FeedEntry.COLUMN_NAME_TITLE));
+               playlists.add(new Playlist(title));
+           }
+       }
+       cursor.close();
+
+       return playlists;
     }
 }
